cb0eb824156ce32a51053c8bdf63706a9815705b
yooju <yoojunyang0323@gmail.com>
2024-05-02 07:03:01 +0800

    this is mainly for creating a new html file for log in page

--- mian.py
+++ mian.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/login')
 def hello():
-    return 'hello'
+    return "hello"
 
 if __name__ == "__main__":
     app.run()
