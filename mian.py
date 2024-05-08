4368b309f5c103a723625b61d14c056e98556d31
yooju <yoojunyang0323@gmail.com>
2024-05-08 11:59:14 +0800

    Trying to connect render_template of homepage.html

--- mian.py
+++ mian.py
@@ -2,11 +2,16 @@ from flask import Flask,  redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/homepage')
+def homepage():
+    return render_template("homepage.html")
+
 @app.route('/login',methods=['POST','GET'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        return redirect(url_for('homepage'))
     else:
         return render_template("loginpage.html")
 
