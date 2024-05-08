1e3bbe82c555347d11c4ddb270d74a43aea101d1
yooju <yoojunyang0323@gmail.com>
2024-05-08 10:59:54 +0800

    Connect the log in page with the home page of mmushop

--- mian.py
+++ mian.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,  redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/login')
-def hello():
-    return "hello"
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    else:
+        return render_template("loginpage.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
