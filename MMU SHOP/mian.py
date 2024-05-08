ea72a1e66db00c57e0d92e10472cc7284c394f03
yooju <yoojunyang0323@gmail.com>
2024-05-08 13:15:18 +0800

    Identify whether the students are mmu students through their gmail

--- MMU SHOP/mian.py
+++ MMU SHOP/mian.py
@@ -2,18 +2,27 @@ from flask import Flask,  redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/homepage')
-def homepage():
-    return render_template("homepage.html")
+mmu_student = False
 
-@app.route('/login',methods=['POST','GET'])
+@app.route('/mmustudent/<name>')
+def mmustudent(name):
+    return render_template("homepage.html" , name=name)
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    global mmu_student
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        return redirect(url_for('homepage'))
+        if "@student.mmu.edu.my" in email:
+            mmu_student = True
+            username = email.split('@')[0]
+            return redirect(url_for('mmustudent', name=username))
+        else:
+            mmu_student = False
+            return "Invalid email domain. Please use an email from '@student.mmu.edu.my'."
     else:
-        return render_template("loginpage.html")
+        return render_template("login.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
